847aa7f27c7bcc3295ea52e607ec58d7da3520e7
Vasilis Nikolaou <basilisnik16@gmail.com>
2021-05-25 16:55:49 +0300

    Added the project files

---
+++ video-chat-app-webrtc/src/main/java/signalingserver/VideoChatAppWebrtcApplication.java
@@ -0,0 +1,13 @@
+package signalingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoChatAppWebrtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoChatAppWebrtcApplication.class, args);
+	}
+
+}
---
+++ video-chat-app-webrtc/src/test/java/signalingserver/VideoChatAppWebrtcApplicationTests.java
@@ -0,0 +1,13 @@
+package signalingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChatAppWebrtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
