2ee47f58f35d4bb86a6ca2be7f539c2beb3ccc96
Vasilis Nikolaou <basilisnik16@gmail.com>
2021-06-14 12:57:50 +0300

    Updated javascript & html files

---
+++ video-chat-app-webrtc/src/main/resources/static/main.js
@@ -0,0 +1,43 @@
+const selfView = document.querySelector('#selfView');
+const audioSelect = document.querySelector('select#audioSource');
+const videoSelect = document.querySelector('select#videoSource');
+
+
+function getDevices(mediaDevices) {
+    mediaDevices.forEach(device => {
+        const option = document.createElement('option');
+        option.value = device.deviceId;
+        option.text = device.label;
+
+        if (device.kind === 'videoinput') {
+            videoSelect.appendChild(option);
+        } else if (device.kind === 'audioinput') {
+            audioSelect.appendChild(option);
+        }
+    });
+}
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices);
+
+async function start() {
+    if (window.stream) {
+        window.stream.getTracks().forEach(track => track.stop());
+    }
+
+    const audioSource = audioSelect.value;
+    const videoSource = videoSelect.value;
+    const constraints = {
+        audio: { deviceId: audioSource },
+        video: { deviceId: videoSource }
+    }
+
+    const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+    window.stream = mediaStream;
+    selfView.srcObject = mediaStream;
+
+};
+
+audioSelect.onchange = start;
+videoSelect.onchange = start;
+
+start();
